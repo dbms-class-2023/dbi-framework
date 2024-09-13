3b54c5eab72141af42876176e9261ee377607364
Dmitry Barashev <dbarashev@ganttproject.biz>
2024-09-13 19:44:09 +0400

    Changed the cache benchmark so that it was clear that aging/clock caches win over fifo

--- src/main/kotlin/net/barashev/dbi2023/app/CacheBenchmark.kt
+++ src/main/kotlin/net/barashev/dbi2023/app/CacheBenchmark.kt
@@ -24,7 +24,9 @@ import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.double
 import com.github.ajalt.clikt.parameters.types.int
 import net.barashev.dbi2023.CachedPage
+import net.barashev.dbi2023.PageId
 import net.barashev.dbi2023.createHardDriveEmulatorStorage
+import org.slf4j.LoggerFactory
 import java.util.*
 import kotlin.math.max
 import kotlin.math.min
@@ -46,11 +48,13 @@ class CacheBenchmark: CliktCommand() {
         val ticketPageCount = accessManager.pageCount("ticket")
 
         val ticketPages = accessManager.createFullScan("ticket").pages().map { it.close(); it.id }.toList()
-        val pageWorkingSet = ticketPages.shuffled().toList()
+        val flightPages = accessManager.createFullScan("flight").pages().map { it.close(); it.id }.toList()
+        val pageWorkingSet = (ticketPages + flightPages).shuffled().toList()
 
-        println("Page count: planet=$planetPages spacecraft=$spacecraftPages flight=$flightPageCount ticket=$ticketPageCount")
-        println(cache.stats)
+        LOG.info("Page count: planet=$planetPages spacecraft=$spacecraftPages flight=$flightPageCount ticket=$ticketPageCount")
+        LOG.info(cache.stats.toString())
         cache.stats.reset()
+        cache.flush()
 
         val accessCost1 = storage.totalAccessCost
         val random = Random().also { it.setSeed(System.currentTimeMillis()) }
@@ -58,13 +62,16 @@ class CacheBenchmark: CliktCommand() {
         var longPinCount = 0
         var maxLongPinCount = 0
 
+        val pageAccessCounts = mutableMapOf<PageId, Int>()
         val pinnedPages = mutableListOf<CachedPage>()
-        repeat(1000) {
+        repeat(2000) {
             // With some probability we will do a full scan of a pretty large table, that will possible replace many
             // pages in the cache.
             if (random.nextDouble() < testFullScanChance) {
-                accessManager.createFullScan("flight").pages().forEach { it.close() }
+                LOG.debug("---- full scan! ----")
+                accessManager.createFullScan("ticket").pages().forEach { it.close() }
                 fullScanCount += 1
+                LOG.debug("---- end of full scan! ----")
             }
             // Sometimes we will close some of the long-pinned pages
             if (pinnedPages.isNotEmpty() && random.nextInt(5) == 1) {
@@ -73,12 +80,20 @@ class CacheBenchmark: CliktCommand() {
                 donePages.clear()
             }
 
-            // Now generate a random page to fetch.
-            val pageNumber = random.nextGaussian(pageWorkingSet.size/2.0, pageWorkingSet.size/2.0).toInt()
-                .let { max(it, 0) }.let { min(it, pageWorkingSet.size - 1) }
+            val pageNumber = if (random.nextBoolean()) {
+                // Now generate a random page to fetch.
+                random.nextGaussian(pageWorkingSet.size / 2.0, pageWorkingSet.size / 4.0).toInt()
+                    .let { max(it, 0) }.let { min(it, pageWorkingSet.size - 1) }
+            } else {
+                random.nextInt(0, pageWorkingSet.size).toInt()
+                    .let { max(it, 0) }.let { min(it, pageWorkingSet.size - 1) }
+            }
 
 
             val pageId = pageWorkingSet[pageNumber]
+            pageAccessCounts.getOrPut(pageId) { 0 }
+            pageAccessCounts[pageId] = pageAccessCounts[pageId]!! + 1
+
             val page = cache.getAndPin(pageWorkingSet[pageNumber])
             // With some probability we will "long pin" the page, as if there is a relatively long transaction
             // that modifies the page.
@@ -93,12 +108,12 @@ class CacheBenchmark: CliktCommand() {
             }
         }
         pinnedPages.forEach { it.close() }
-        println("Access cost: ${storage.totalAccessCost - accessCost1}")
-        println(cache.stats)
-        println("Test stats: #full scans=$fullScanCount #long pins=$longPinCount, max long pin count=$maxLongPinCount")
-
-
-
+        LOG.debug("page access: ${pageAccessCounts.entries.sortedBy { it.value }.reversed()}")
+        LOG.debug("totally we requested ${pageAccessCounts.size} random pages")
+        LOG.info("Access cost: ${storage.totalAccessCost - accessCost1}")
+        LOG.info(cache.stats.toString())
+        LOG.info("Test stats: #full scans=$fullScanCount #long pins=$longPinCount, max long pin count=$maxLongPinCount")
     }
-
 }
+
+private val LOG = LoggerFactory.getLogger("Cache.Benchmark")
