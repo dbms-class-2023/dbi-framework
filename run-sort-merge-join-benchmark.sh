183884bc63c9733d6045a52e0a8f07849d253344
Dmitry Barashev <dbarashev@ganttproject.biz>
2024-09-27 20:35:10 +0400

    Compare the default and submitted sort implementations

--- run-sort-merge-join-benchmark.sh
+++ run-sort-merge-join-benchmark.sh
@@ -1,8 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 DEFAULT_COST=$(./gradlew run --args='join-benchmark --cache-impl clock --cache-size 70 --data-scale 10 --join-algorithm merge' | grep "JOIN COST" | cut -d ":" -f 2)
 echo "Join cost with the DEFAULT (SLOW) merge sort implementation: $DEFAULT_COST"
 
 REAL_COST=$(./gradlew run --args='join-benchmark --cache-impl clock --cache-size 70 --data-scale 10 --join-algorithm merge --real-sort' | grep "JOIN COST" | cut -d ":" -f 2)
-echo "Join cost with the PROVIDED (hopefully FAST) merge sort implementation: $REAL_COST"
+echo "Join cost with the SUBMITTED (hopefully FAST) merge sort implementation: $REAL_COST"
+
+EXPR="${DEFAULT_COST}/10 < ${REAL_COST}"
+echo "$EXPR ?"
+if (($(bc <<< "$EXPR") == 1)); then
+    echo "Yes. It appears that the SUBMITTED implementation may be improved!";
+    exit 1;
+fi
